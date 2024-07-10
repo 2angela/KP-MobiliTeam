dd87e7ddac2daa166ebb6cf31978b4b4441dc84c
estherreginaaa <sthrregina@gmail.com>
2024-07-10 14:19:33 +0700

    feat: font

--- App.js
+++ App.js
@@ -3,19 +3,36 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "./app/Home";
 import Landing from "./app/Landing";
+import Notice from "./app/Notice";
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Landing"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Landing" component={Landing}></Stack.Screen>
-        <Stack.Screen name="Home" component={Home}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  const [fontsLoaded] = useFonts({
+    MontserratThin: Montserrat_300Regular,
+    MonserratRegular: Montserrat_400Regular,
+    MontserratMedium: Montserrat_500Medium,
+    MontserratBold: Montserrat_700Bold,
+    MontserratExtraBold: Montserrat_800ExtraBold,
+    MontserratSemiBold: Montserrat_600SemiBold,
+  });
+  if (!fontsLoaded) {
+    console.log("Loading fonts...");
+    return null;
+  } else {
+    console.log("Fonts Loaded!");
+
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Landing"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Landing" component={Landing}></Stack.Screen>
+          <Stack.Screen name="Home" component={Home}></Stack.Screen>
+          <Stack.Screen name="Notice" component={Notice}></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }

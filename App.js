59baef6160f1ec9d9a42fb24fc0d9c288bc839ae
estherreginaaa <sthrregina@gmail.com>
2024-07-10 14:29:30 +0700

    fix: font

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Landing from "./app/Landing";
 import Notice from "./app/Notice";
 import {
   useFonts,
-  Montserrat_300Thin,
+  Montserrat_300Light,
   Montserrat_400Regular,
   Montserrat_500Medium,
   Montserrat_600SemiBold,
@@ -17,7 +17,7 @@ const Stack = createStackNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    MontserratThin: Montserrat_300Thin,
+    MontserratThin: Montserrat_300Light,
     MonserratRegular: Montserrat_400Regular,
     MontserratMedium: Montserrat_500Medium,
     MontserratBold: Montserrat_700Bold,

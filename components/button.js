41082ad6f943c3f66b2cdeb08c7ba5cd2e42af70
angela <angelageraldine2@gmail.com>
2024-07-11 13:58:59 +0700

    feat: button component

---
+++ components/button.js
@@ -0,0 +1,45 @@
+import { Text, StyleSheet, Pressable } from "react-native";
+
+export default function Button({ label, action }) {
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        styles.buttonStyle,
+        pressed ? styles.buttonPressed : null,
+      ]}
+      onPress={action}
+    >
+      <Text style={styles.buttonText}>{label}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonText: {
+    fontFamily: "MontserratBold",
+    fontSize: 16,
+    color: "white",
+  },
+  buttonStyle: {
+    display: "flex",
+    alignItems: "center",
+    width: "80%",
+    paddingVertical: 10,
+    backgroundColor: "rgb(59, 59, 137)",
+    borderRadius: 30,
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowRadius: 4,
+    elevation: 4,
+    shadowOpacity: 1,
+    marginTop: 20,
+    marginBottom: 40,
+  },
+  buttonPressed: {
+    backgroundColor: "#B1B1D0",
+    shadowOpacity: 0,
+  },
+});

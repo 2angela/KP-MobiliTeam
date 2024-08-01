16659f3b443b4f7640bb50bc929499a439a6aec7
unknown <sthrregina@gmail.com>
2024-08-01 14:53:55 +0700

    chore: move component modal to navigation and component modaleditprofile to profile

--- components/navigation.js
+++ components/navigation.js
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text, Pressable } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Pressable,
+  Alert,
+  Modal,
+  TextInput,
+  TouchableWithoutFeedback,
+} from "react-native";
 import Home from "../assets/icons/home_fill.svg";
 import Profile from "../assets/icons/account-circle.svg";
 import Bell from "../assets/icons/bell_fill.svg";
-import NotificationModal from "./modal";
 
 export default function Navigation({
   navigation,
@@ -76,6 +84,51 @@ export default function Navigation({
     </View>
   );
 }
+
+const NotificationModal = ({ visible, onClose }) => {
+  const [notification, setNotification] = useState("");
+
+  return (
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={visible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          onClose();
+        }}
+      >
+        <View style={styles.overlay}>
+          <TouchableWithoutFeedback onPress={onClose}>
+            <View style={styles.centeredView}>
+              <TouchableWithoutFeedback>
+                <View style={styles.modalView}>
+                  <Text style={styles.modalText}>Send a notification</Text>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Message (optional)"
+                    value={notification}
+                    onChangeText={setNotification}
+                  />
+                  <Pressable
+                    style={({ pressed }) => [
+                      styles.buttonStyle,
+                      pressed ? styles.buttonPressed : null,
+                    ]}
+                    onPress={onClose}
+                  >
+                    <Text style={styles.buttonText}>Send</Text>
+                  </Pressable>
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+      </Modal>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   shadowContainer: {
@@ -159,5 +212,75 @@ const styles = StyleSheet.create({
     fontFamily: "MontserratRegular",
     fontSize: 12,
     textAlign: "center",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    paddingBottom: 20,
+    paddingTop: 20,
+    paddingLeft: 15,
+    paddingRight: 15,
+    alignItems: "flex-start",
+    elevation: 4,
+    shadowRadius: 4,
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+  },
+  buttonStyle: {
+    display: "flex",
+    alignItems: "center",
+    width: 350,
+    paddingVertical: 15,
+    backgroundColor: "rgb(59, 59, 137)",
+    borderRadius: 30,
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowRadius: 4,
+    elevation: 4,
+    shadowOpacity: 1,
+  },
+  buttonPressed: {
+    backgroundColor: "#B1B1D0",
+    shadowOpacity: 0,
+  },
+  buttonText: {
+    fontFamily: "MontserratBold",
+    fontSize: 16,
+    color: "white",
+  },
+  modalText: {
+    fontFamily: "MontserratMedium",
+    fontSize: 14,
+    marginBottom: 15,
+    textAlign: "left",
+  },
+  input: {
+    height: 50,
+    width: 350,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 8,
+    borderRadius: 8,
+    textAlign: "left",
   },
 });

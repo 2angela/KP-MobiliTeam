4bde57dcc29e595022c5715d707a0084a9b2eaf8
unknown <sthrregina@gmail.com>
2024-08-13 14:20:43 +0700

    fix: gradient background

--- components/gradientBG.js
+++ components/gradientBG.js
@@ -1,21 +1,25 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions, View } from "react-native";
 import { Fragment } from "react";
 import BG from "../assets/bg.svg";
 
 export default function GradientBG() {
+  const originalWidth = 393;
+  const originalHeight = 1063;
+  const aspectRatio = originalWidth / originalHeight;
+  const windowWidth = Dimensions.get("window").width;
   return (
-    <Fragment>
-      <BG style={styles.background} resizeMode="cover" />
-    </Fragment>
+    <View style={[styles.background, { width: windowWidth, aspectRatio }]}>
+      <BG
+        width="100%"
+        height="100%"
+        viewBox={`0 0 ${originalWidth} ${originalHeight}`}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   background: {
     position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
   },
 });
